eec3ab039ae50295a107155e32fe729924a72c11
Dario <dario.lohmuller@students.fhnw.ch>
2019-05-09 18:50:50 +0200

    Triggerables created

---
+++ Assets/Scripts/BridgeBlockade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeBlockade : MonoBehaviour, ITriggerable
+{
+    private Animator animator;
+    private bool isActive = false;
+    public bool IsActive
+    {
+        get { return isActive; }
+        set
+        {
+            if (value) {
+                animator.SetTrigger("Appear");
+            }
+            else
+            {
+                animator.SetTrigger("Disappear");
+            }
+            
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+}
