ac69b5823d337865bc3276ac9f76f9fdeb877ae2
Dario <dario.lohmuller@students.fhnw.ch>
2019-05-13 11:45:49 +0200

    Cube added

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour, ITriggerable
+{
+    private Vector3 initPosition;
+    private Animator animator;
+    private bool isActive = false;
+    public bool IsActive
+    {
+        get { return isActive; }
+        set
+        {
+            if (value)
+            {
+                animator.SetTrigger("Despawn");
+            }
+            isActive = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPosition = transform.position;
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("Spawn");
+    }
+
+    public void OnDespawnAnimationEnd()
+    {
+        transform.position = initPosition;
+        animator.SetTrigger("Spawn");
+    }
+}
