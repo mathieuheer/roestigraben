0cee8b4abef02558b743b177c953ade294bb8ecf
Benjamin Denzler <benjamin.denzler@students.fhnw.ch>
2019-06-04 15:20:56 +0200

    loadButton MainMenu WIP

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -3,33 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 
-public class MainMenu : MonoBehaviour
+public class MainMenu : PauseMenu
 {
-
-    public Player player;
 
     public void NewGame(){
         SceneManager.LoadScene(1);
     }
 
-    public void Load(){
+    public override void Load(){
 
-        PlayerData data = SaveSystem.LoadPlayer();
+        SceneManager.LoadScene(1);
 
-        player.health = data.health;
-        player.numOfKeys = data.numOfKeys;
-
-        Vector3 position;
-        position.x = data.position[0];
-        position.y= data.position[1];
-        position.z = data.position[2];
-        player.transform.position = position;
-
-        player.UpdateHearts();
-
-        player.GetComponent<Renderer>().enabled = true;
-        player.gameObject.SetActive(true);
-
+        base.Load();
     }
 
     public void Exit(){
