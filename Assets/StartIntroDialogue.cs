6cba602ed04c5f6f9f089bab24cce66638ed6ec4
Mathieu Heer <mathieu.heer@students.fhnw.ch>
2019-06-07 21:45:47 +0200

    add intro dialogue

---
+++ Assets/StartIntroDialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartIntroDialogue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private bool dialogue = false;
+    public float targetTime = 1f;
+
+    void Update()
+    {
+
+        targetTime -= Time.deltaTime;
+
+        if (!dialogue && targetTime <= 0.0f)
+        {
+            dialogue = true;
+            TimerEnded();
+        }
+
+    }
+
+    void TimerEnded()
+    {
+        GetComponent<DialogueTrigger>().TriggerDialogue();
+    }
+
+}
